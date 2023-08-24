810de3bd1e7918a40f0235203bb7278782d3082f
Matt Eva <matteweva@gmail.com>
2023-08-23 20:41:02 -0700

    creating users successfully in seeds

--- dist/db/seeds.js
+++ dist/db/seeds.js
@@ -14,53 +14,82 @@ catch (err) {
     console.log(`-- Connection error --\n${err}\n-- Cause --\n${err}`);
 }
 const session = driver.session();
-try {
-    console.log("deleting users");
-    const deleteUsers = "MATCH (u:User) DETACH DELETE u";
-    let transaction = await session.beginTransaction();
-    console.log("starting transaction");
-    await transaction.run(deleteUsers);
-    console.log("users deleted");
-    await transaction.close();
-    console.log("transaction closed");
-}
-catch (error) {
-    console.error(error);
-}
-try {
-    console.log("deleting messages");
-    const deleteMessages = "MATCH (m:Message) DETACH DELETE m";
-    let transaction = await session.beginTransaction();
-    await transaction.run(deleteMessages);
-    await transaction.close();
-    console.log("messages deleted");
-}
-catch (error) {
-    console.error(error);
-}
-try {
-    console.log("deleting chats");
-    const deleteChats = "MATCH (c:Chat) DETACH DELETE c";
-    let transaction = await session.beginTransaction();
-    await transaction.run(deleteChats);
-    await transaction.close();
-    console.log("chats deleted");
-}
-catch (error) {
-    console.error(error);
-}
-// Verify Deletion
-try {
-    console.log("verifying deletes");
-    const checkDeleted = "MATCH (n:Chat|Message|User) RETURN n";
-    let transaction = await session.beginTransaction();
-    const emptyResults = await transaction.run(checkDeleted);
-    await transaction.close();
-    console.log(emptyResults);
-}
-catch (error) {
-    console.error(error);
-}
+const deleteSeeds = async () => {
+    try {
+        console.log("deleting users");
+        const deleteUsers = "MATCH (u:User) DETACH DELETE u";
+        let transaction = await session.beginTransaction();
+        console.log("starting transaction");
+        await transaction.run(deleteUsers);
+        console.log("users deleted");
+        await transaction.close();
+        console.log("transaction closed");
+    }
+    catch (error) {
+        console.error(error);
+    }
+    try {
+        console.log("deleting messages");
+        const deleteMessages = "MATCH (m:Message) DETACH DELETE m";
+        let transaction = await session.beginTransaction();
+        await transaction.run(deleteMessages);
+        await transaction.close();
+        console.log("messages deleted");
+    }
+    catch (error) {
+        console.error(error);
+    }
+    try {
+        console.log("deleting chats");
+        const deleteChats = "MATCH (c:Chat) DETACH DELETE c";
+        let transaction = await session.beginTransaction();
+        await transaction.run(deleteChats);
+        await transaction.close();
+        console.log("chats deleted");
+    }
+    catch (error) {
+        console.error(error);
+    }
+    // Verify Deletion
+    try {
+        console.log("verifying deletes");
+        const checkDeleted = "MATCH (n:Chat|Message|User) RETURN n";
+        let transaction = await session.beginTransaction();
+        const emptyResults = await transaction.run(checkDeleted);
+        await transaction.close();
+        console.log(emptyResults.records);
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
+// await deleteSeeds()
+const testCreate = async () => {
+    try {
+        const addUser = "CREATE (u:User) RETURN u";
+        let transaction = await session.beginTransaction();
+        const results = await transaction.run(addUser);
+        await transaction.close();
+        console.log(results.records);
+    }
+    catch (e) {
+        console.error(e);
+    }
+};
+const checkCreate = async () => {
+    try {
+        const checkUser = "MATCH (u:User) RETURN u";
+        let transaction = await session.beginTransaction();
+        const results = await transaction.run(checkUser);
+        await transaction.close();
+        console.log(results.records);
+    }
+    catch (e) {
+        console.error(e);
+    }
+};
+await testCreate();
+await checkCreate();
 // User Seeds
 const users = [
     {
@@ -83,7 +112,7 @@ const createUser = async (user) => {
         let transaction = await session.beginTransaction();
         const results = await transaction.run(addUser, user);
         await transaction.close();
-        console.log(results);
+        console.log(results.records);
     }
     catch (e) {
         console.error(e);
@@ -92,6 +121,35 @@ const createUser = async (user) => {
 for (const user of users) {
     await createUser(user);
 }
+try {
+    const checkUsers = "MATCH (u:User) RETURN u";
+    let transaction = await session.beginTransaction();
+    const results = await transaction.run(checkUsers);
+    await transaction.close();
+    console.log(results);
+}
+catch (error) {
+    console.error(error);
+}
 // Chat Seeds
+// const chat = {
+//   id: uuid()
+// }
+// try {
+//   const createChatWithUsers = `
+//     MATCH (a:User), (b:User) 
+//     WHERE a.name='Matt' AND b.name = 'Wills' 
+//     CREATE (c:Chat {id: $id}), (a) - [m:IN_CHAT]->(c), (b) - [n:IN_CHAT] -> (c) 
+//     RETURN c, m, n
+//   `
+//   let transaction = await session.beginTransaction()
+//   const results = await transaction.run(createChatWithUsers, chat)
+//   await transaction.close()
+//   console.log("relationship results", results)
+// } catch(e) {
+//   console.error(e)
+// }
 // Message Seeds
+await session.close();
+await driver.close();
 //# sourceMappingURL=seeds.js.map
